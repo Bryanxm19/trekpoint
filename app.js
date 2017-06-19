07ddf2e8839586c0032f557c0e7404b816668e10
Bryan Mateer <bryanxmateer@gmail.com>
2017-06-19 20:51:51 +0000

    Configure passport and create search route

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
     mongoose        = require("mongoose"),
     flash           = require("connect-flash"),
     passport        = require("passport"),
-    LocalStrategy   = require("passport-local");
+    LocalStrategy   = require("passport-local"),
+    User            = require("./models/user");
     
 // routes config
 var indexRoutes = require("./routes/index");
@@ -18,9 +19,28 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 //passport config
+app.use(require("express-session")({
+    secret: "Anything",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
 
 // use routes
 app.use(indexRoutes);
+
+app.get("/search", function(req, res) {
+    res.send("hey");
+})
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server started...");
