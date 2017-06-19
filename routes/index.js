2a5e3169b672aed9263724e5d41a2d3614ec7f9b
Bryan Mateer <bryanxmateer@gmail.com>
2017-06-19 16:43:23 +0000

    Create route for login/sign up

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get("/", function(req, res) {
     res.render("index");
 });
 
+// login and sign up page
+router.get("/login", function(req, res) {
+    res.render("login");
+});
+
 module.exports = router;
