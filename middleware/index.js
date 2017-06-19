66ec201a2cbab95e294e2a8ba1eaa163178bb0ce
Bryan Mateer <bryanxmateer@gmail.com>
2017-06-19 20:49:59 +0000

    Create middleware file

---
+++ middleware/index.js
@@ -0,0 +1,18 @@
+var middlewareObj = {};
+
+middlewareObj.loginConfirm = function(req, res, next) {
+    if(req.body.password != req.body.second_password) {
+        console.log("Passwords do not match");
+        res.redirect("/");
+    } 
+    else if(req.body.password.length < 6) {
+        console.log("Password must be 6 characters");
+        res.redirect("/");
+    }
+    else {
+        next();
+    }
+}
+
+module.exports = middlewareObj;
+
