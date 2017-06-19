4e57efb32592934195625d37a651a6d4533240a9
Bryan Mateer <bryanxmateer@gmail.com>
2017-06-19 20:52:29 +0000

    Create post routes for login and signup

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
-var express = require("express"),
-    router  = express.Router();
+var express     = require("express"),
+    router      = express.Router(),
+    passport    = require("passport"),
+    middleware  = require("../middleware"),
+    User        = require("../models/user");
     
 // root
 router.get("/", function(req, res) {
     res.render("index");
 });
 
-// login and sign up page
-router.get("/login", function(req, res) {
-    res.render("login");
+// login
+router.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/search",
+        failureRedirect: "/"
+    })
+);
+
+// sign up
+router.post("/register", middleware.loginConfirm, function(req, res) {
+    User.register(new User({email: req.body.email, username: req.body.username}), req.body.password, function(err, user) {
+        if(err) {
+            console.log(err);
+            res.redirect("/");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/search"); 
+        });
+    });
 });
 
 module.exports = router;
