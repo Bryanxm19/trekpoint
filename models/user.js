ebac5a9b640a4c829f75e807fdf566b368a1f769
Bryan Mateer <bryanxmateer@gmail.com>
2017-06-20 14:45:49 +0000

    Add unique validator

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 var mongoose            = require("mongoose"),
-passportLocalMongoose   = require("passport-local-mongoose");
+passportLocalMongoose   = require("passport-local-mongoose"),
+uniqueValidator         = require('mongoose-unique-validator');
 
 var userSchema = new mongoose.Schema({
    email: {
        type: String,
+       unique: true,
        required: true
    },
    username: {
@@ -16,5 +18,6 @@ var userSchema = new mongoose.Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(uniqueValidator, { message: 'A user with the given {PATH} is already registered' });
 
 module.exports = mongoose.model("User", userSchema);
