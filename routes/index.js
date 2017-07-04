a1317f1816f143c84d4ab5e09e9a0a92848a366d
Bryan Mateer <bryanxmateer@gmail.com>
2017-07-04 01:02:03 +0000

    Require new packages and use promises for returning results from the api calls

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express     = require("express"),
     router      = express.Router(),
     passport    = require("passport"),
+    request     = require("request-promise"),
+    Promise     = require("bluebird"),
     middleware  = require("../middleware"),
     helper      = require("../helpers/helper"),
     User        = require("../models/user");
@@ -42,6 +44,26 @@ router.get("/logout", function(req, res) {
 
 router.get("/search", middleware.isLoggedIn, function(req, res) {
     res.render("search");
+});
+
+router.get("/activities", middleware.isLoggedIn, function(req, res) {
+    var destination = req.query.destination;
+    var startDate = req.query.daterange.replace(/\s+/g, '').split("to")[0];
+    var endDate = req.query.daterange.replace(/\s+/g, '').split("to")[1];
+    var activities =  req.query.activities;
+    var results = [];
+    
+    var requests = helper.buildRequest(activities, startDate, endDate, destination);
+    
+    Promise.map(requests, function(obj) {
+      return request(obj).then(function(body) {
+        return JSON.parse(body);
+      });
+    }).then(function(response) {
+      console.log(helper.buildResult(response, activities));
+    }, function(err) {
+      console.log(err);
+    });
 });
 
 module.exports = router;
