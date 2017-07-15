6cd1e92485f7c1b15e7db9177d6adbe2be8d14a8
Bryan Mateer <bryanxmateer@gmail.com>
2017-07-15 02:42:54 +0000

    Handle building requests and responses for api calls

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(express.static(__dirname + "/public"));
 
 //passport config
 app.use(require("express-session")({
-    secret: "Anything",
-    resave: false,
-    saveUninitialized: false
+  secret: "Anything",
+  resave: false,
+  saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -31,13 +31,13 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next) {
-    res.locals.currentUser = req.user;
-    next();
+  res.locals.currentUser = req.user;
+  next();
 });
 
 // use routes
 app.use(indexRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("Server started...");
+  console.log("Server started...");
 });
--- helpers/helper.js
+++ helpers/helper.js
@@ -3,116 +3,129 @@ var moment = require("moment");
 var helperObj = {};
 
 helperObj.checkUserError = function(err) {
-    if(err.message === "A user with the given username is already registered") {
-        return err.message;
-    } else {
-        return err.errors.email.message;
-    }
+  if(err.message === "A user with the given username is already registered") {
+    return err.message;
+  } else {
+    return err.errors.email.message;
+  }
 };
 
-helperObj.buildRequest = function(choices, startDate, endDate, destination) {
-    var request = [];
-    var ticketmasterURL = "https://app.ticketmaster.com/discovery/v2/events.json";
-    var ticketmasterQueries = {
-        apikey: process.env.TICKETMASTER_KEY,
-        sort: "date,asc",
-        source: "ticketmaster",
-        startDateTime: startDate + "T12:00:00Z",
-        endDateTime: endDate + "T23:59:59Z",
-        city: destination
-    };
-    if(choices.indexOf('music') != -1) {
-        request.push({
-            url: ticketmasterURL,
-            qs: {
-                apikey: ticketmasterQueries.apikey,
-                sort: ticketmasterQueries.sort,
-                source: ticketmasterQueries.source,
-                startDateTime: ticketmasterQueries.startDateTime,
-                endDateTime: ticketmasterQueries.endDateTime,
-                city: ticketmasterQueries.city,
-                keyword: "music"
-            }
-        }); 
-    }
-    if(choices.indexOf('sports') != -1) {
-        request.push({
-            url: ticketmasterURL,
-            qs: {
-                apikey: ticketmasterQueries.apikey,
-                sort: ticketmasterQueries.sort,
-                source: ticketmasterQueries.source,
-                startDateTime: ticketmasterQueries.startDateTime,
-                endDateTime: ticketmasterQueries.endDateTime,
-                city: ticketmasterQueries.city,
-                keyword: "sports"
-            }
-        }); 
-    }
-    if(choices.indexOf('arts') != -1) {
-        request.push({
-            url: ticketmasterURL,
-            qs: {
-                apikey: ticketmasterQueries.apikey,
-                sort: ticketmasterQueries.sort,
-                source: ticketmasterQueries.source,
-                startDateTime: ticketmasterQueries.startDateTime,
-                endDateTime: ticketmasterQueries.endDateTime,
-                city: ticketmasterQueries.city,
-                keyword: "theater"
-            }
-        }); 
-    }
-    return request;
+helperObj.buildRequest = function(data) {
+  var request = [];
+  var ticketmasterURL = "https://app.ticketmaster.com/discovery/v2/events.json";
+  var ticketmasterQueries = {
+    apikey: process.env.TICKETMASTER_KEY,
+    sort: "date,asc",
+    source: "ticketmaster",
+    startDateTime: data.startDate + "T12:00:00Z",
+    endDateTime: data.endDate + "T23:59:59Z",
+    city: data.destination
+  };
+  if(data.activities.indexOf('music') != -1) {
+    request.push({
+      url: ticketmasterURL,
+      qs: {
+        apikey: ticketmasterQueries.apikey,
+        sort: ticketmasterQueries.sort,
+        source: ticketmasterQueries.source,
+        startDateTime: ticketmasterQueries.startDateTime,
+        endDateTime: ticketmasterQueries.endDateTime,
+        city: ticketmasterQueries.city,
+        keyword: "music"
+      }
+    }); 
+  }
+  if(data.activities.indexOf('sports') != -1) {
+    request.push({
+      url: ticketmasterURL,
+      qs: {
+        apikey: ticketmasterQueries.apikey,
+        sort: ticketmasterQueries.sort,
+        source: ticketmasterQueries.source,
+        startDateTime: ticketmasterQueries.startDateTime,
+        endDateTime: ticketmasterQueries.endDateTime,
+        city: ticketmasterQueries.city,
+        keyword: "sports"
+      }
+    }); 
+  }
+  if(data.activities.indexOf('arts') != -1) {
+    request.push({
+      url: ticketmasterURL,
+      qs: {
+        apikey: ticketmasterQueries.apikey,
+        sort: ticketmasterQueries.sort,
+        source: ticketmasterQueries.source,
+        startDateTime: ticketmasterQueries.startDateTime,
+        endDateTime: ticketmasterQueries.endDateTime,
+        city: ticketmasterQueries.city,
+        keyword: "theater"
+      }
+    }); 
+  }
+  if(data.activities.indexOf('restaurants') != -1) {
+    request.push({
+      url: "",
+      qs: {
+
+      }
+    }); 
+  }
+  return request;
 };
 
 //create events from ticketmaster api
 helperObj.createTMEvents = function(events, eventType) {
-    var results = [];
-    events.forEach(function(event) {
-        var date = moment(new Date(event["dates"]["start"]["localDate"])).format('MMMM Do YYYY'); 
-        var name = event["name"];
-        var url = event["url"];
-        var image_url = event["images"][0]["url"];
-        var venue = event["_embedded"]["venues"][0]["name"];
-        var data = {date: date, name: name, url: url, image_url: image_url, venue: venue, eventType: eventType};
-        results.push(data);
-    });     
-    return results;
+  var results = new Object();
+  results[eventType] = [];
+  
+  events.forEach(function(event) {
+    var date = moment(new Date(event["dates"]["start"]["localDate"])).format('MMMM Do YYYY'); 
+    var name = event["name"];
+    var url = event["url"];
+    var image_url = event["images"][0]["url"];
+    var venue = event["_embedded"]["venues"][0]["name"];
+    var data = {date: date, name: name, url: url, image_url: image_url, venue: venue};
+    results[eventType].push(data);
+  });     
+  return results;
 };
 
-helperObj.buildResult = function(response, activities) {
-    var results = [];
-    var usedChoices =[];
-    var This = this;
-    var events;
-    response.forEach(function(result) {
-        if(activities.indexOf('music') != -1 && usedChoices.indexOf("music") === -1) {
-            usedChoices.push("music");
-            // if music events exist for this request
-            if(result["_embedded"]) {
-                events = result["_embedded"]["events"];
-                results.push.apply(results, This.createTMEvents(events, "music"));
-            }
-        }
-        else if(activities.indexOf('sports') != -1 && usedChoices.indexOf("sports") === -1) {
-            usedChoices.push("sports");
-            // if sports events exist for this request
-            if(result["_embedded"]) {
-                events = result["_embedded"]["events"];
-                results.push.apply(results, This.createTMEvents(events, "sports"));
-            }
-        }
-        else if(activities.indexOf('arts') != -1 && usedChoices.indexOf("arts") === -1) {
-            usedChoices.push("arts");
-            // if arts&theater events exist for this request
-            if(result["_embedded"]) {
-                events = result["_embedded"]["events"];
-                results.push.apply(results, This.createTMEvents(events, "arts"));
-            }
-        } 
-    });
-    return results;
+helperObj.buildResult = function(response, activities, location) {
+  var results = {};
+  var usedChoices =[];
+  var This = this;
+  var events;
+  response.forEach(function(result) {
+    if(activities.indexOf('music') != -1 && usedChoices.indexOf("music") === -1) {
+      usedChoices.push("music");
+      // if music events exist for this request
+      if(result["_embedded"]) {
+        events = result["_embedded"]["events"];
+        Object.assign(results, This.createTMEvents(events, "music"));
+      }
+    }
+    else if(activities.indexOf('sports') != -1 && usedChoices.indexOf("sports") === -1) {
+      usedChoices.push("sports");
+      // if sports events exist for this request
+      if(result["_embedded"]) {
+        events = result["_embedded"]["events"];
+        Object.assign(results, This.createTMEvents(events, "sports"));
+      }
+    }
+    else if(activities.indexOf('arts') != -1 && usedChoices.indexOf("arts") === -1) {
+      usedChoices.push("arts");
+      // if arts&theater events exist for this request
+      if(result["_embedded"]) {
+        events = result["_embedded"]["events"];
+        Object.assign(results, This.createTMEvents(events, "arts"));
+      }
+    }
+    else if(activities.indexOf('restaurants') != -1 && usedChoices.indexOf("restaurants") === -1) {
+      usedChoices.push("restaurants");
+    }        
+  });
+  return Object.assign({}, results);
 };
 
 module.exports = helperObj;
