8e90af405f1a7c8fbf6b16b66768009da6ed4492
Bryan Mateer <bryanxmateer@gmail.com>
2017-07-15 18:44:23 +0000

    Update db url

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ var express         = require("express"),
 var indexRoutes = require("./routes/index");
 
 // connect to db
-// mongoose.connect("mongodb://localhost/trekpoint");
-mongoose.connect("mongodb://bryan:q06362@ds145639.mlab.com:45639/trekpoint");
+mongoose.connect(process.env.DATABASEURL);
 
 // app config
 app.use(bodyParser.urlencoded({extended: true}));
